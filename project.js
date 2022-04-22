304fa66d89a04d007d8522bf3e9e8da0497fc7ce
Asantae Shelton <64320195+Asantae@users.noreply.github.com>
2022-04-22 16:16:51 -0400

    multipage update
    updated the js files associated with the index, contact, and project pages.
    started on the about page
    still need to finish making the contact form and some of the project page elements fully functional.

---
+++ project.js
@@ -0,0 +1,21 @@
+/* Modal Functionality */
+
+const modalOneOpen = document.getElementById('image-button-one');
+const modalOneContainer = document.getElementById('modal-one-container');
+const modalOneClose = document.getElementById('modal-one-close');
+modalOneOpen.addEventListener('click', () => {
+    modalOneContainer.classList.add('show');
+});
+modalOneClose.addEventListener('click', () => {
+    modalOneContainer.classList.remove('show');
+});
+
+const modalTwoOpen = document.getElementById('image-button-two');
+const modalTwoContainer = document.getElementById('modal-two-container');
+const modalTwoClose = document.getElementById('modal-two-close');
+modalTwoOpen.addEventListener('click', () => {
+    modalTwoContainer.classList.add('show');
+});
+modalTwoClose.addEventListener('click', () => {
+    modalTwoContainer.classList.remove('show');
+});
